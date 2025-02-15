1f921ac421115e407115db80bdbc2090d4753758
PrimaSanghvi <primasanghvi905@gmail.com>
2025-02-14 20:00:43 -0500

    procfile error fix

--- website/backend/backend/settings.py
+++ website/backend/backend/settings.py
@@ -138,6 +138,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
-    # "http://localhost:3000",  # React frontend
-    "https://canopy-fund-allocation-7ah6.vercel.app/", 
+     "http://localhost:3000",  # React frontend
+    #"https://canopy-fund-allocation-7ah6.vercel.app/", 
 ]
